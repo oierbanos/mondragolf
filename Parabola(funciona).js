21914b388cad6651a2f982801c4b25744cc01845
AnderSantos7 <56338314+AnderSantos7@users.noreply.github.com>
2019-11-26 11:38:35 +0100

    Add files via upload

--- Parabola(funciona).js
+++ Parabola(funciona).js
@@ -4,7 +4,9 @@ var height = window.innerHeight;
 var canvas = ctx = false;
 var frameRate = 1/40; // Seconds
 var frameDelay = frameRate * 1000; // ms
-var loopTimer = false;
+
+var img = new Image();
+	img.src = 'api.png';
 
 
 /*
@@ -20,9 +22,10 @@ var loopTimer = false;
  * beach ball: mass 0.05, radius 30
  * lead ball: mass 10, restitution -0.05
  */
+ 
 var ball = {
-    position: {x: 0, y: 300},
-    velocity: {x: 10, y: 0},
+    position: {x: 100, y: height-40},
+    velocity: {x: 0, y: 0},
     mass: 0.045, //kg
     radius: 4.3, // 1px = 1cm
     restitution: -0.35
@@ -42,16 +45,28 @@ function getMousePosition(e) {
     mouse.y = e.pageY;
 }
 
-var mouseDown = function(e) {
-    if (e.which == 1) {
-        getMousePosition(e);
-        mouse.isDown = true;
-        mouseclick.x = e.pageX;
-  		mouseclick.y = e.pageY;
-    }
+function onloadfuntzioak(){
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	height = canvas.height;
+	width = canvas.width;
+
+	ctx.drawImage(img, 0, canvas.height-150, 900, 150);
+	
+	
 }
-var mouseUp = function(e) { 
-    if (e.which == 1) {
+
+var mouseDown = function(e) {
+	if(e.which == 1 && ball.velocity.x == 0) {
+		getMousePosition(e);
+		mouse.isDown = true;
+		mouseclick.x = e.pageX;
+		mouseclick.y = e.pageY;
+		marra == true;
+	}
+}
+var mouseUp = function(e) {
+    if (e.which == 1 && ball.velocity.x == 0) {
         mouse.isDown = false;
         ball.velocity.y = (mouseclick.y - mouse.y) /20;
         ball.velocity.x = (mouseclick.x - mouse.x) /20;
@@ -63,31 +78,35 @@ var setup = function() {
     ctx = canvas.getContext("2d");
     
     canvas.onmousemove = getMousePosition;
-    canvas.onmousedown = mouseDown;
+	canvas.onmousedown = mouseDown;
     canvas.onmouseup = mouseUp;
-	canvas.height = height;
-	canvas.width = width;
+	
 	
 	setInterval(rodarsuelo, 15); //Que la función se haga cada 15ms
 	
-	
-    
     ctx.fillStyle = 'red';
     ctx.strokeStyle = '#000000';
+
     loopTimer = setInterval(loop, frameDelay);
 }
 
 //Reducir velocidad cuando no bota
 function rodarsuelo(){
-	if (ball.position.y > height-101 - ball.radius){ //Cuando esté en entre el suelo y la altura de 1px
-		ball.velocity.x *= 0.98; //Se hace solo x0.98 porque se hace cada 15ms, cuanto mayor sea el intervalo, menor la multiplicación, se haría x 0.95 (por ejemplo)
+	
+	if (ball.position.y > height-41 - ball.radius){ //Cuando esté en entre el suelo y la altura de 1px
+		ball.velocity.x *= 0.97; //Se hace solo x0.97 porque se hace cada 15ms, cuanto mayor sea el intervalo, menor la multiplicación, se haría x 0.95 (por ejemplo)
 	}
+	if (ball.velocity.x == 0){
+		ingame = false;
+    }
+	else ingame = true;
 }
+
 	
 var loop = function() {
     if ( ! mouse.isDown) {
 		
-            // Calcular fuerzas: Fd = -1/2 * ResisAire * A * rho * v^2
+        // Calcular fuerzas: Fd = -1/2 * ResisAire * A * rho * v^2
         var Fx = -0.5 * ResisAire * A * rho * Math.abs(ball.velocity.x * ball.velocity.x);
         var Fy = -0.5 * ResisAire * A * rho * Math.abs(ball.velocity.y * ball.velocity.y);
         
@@ -106,45 +125,50 @@ var loop = function() {
     }
 	
     // Collisiones
-    if (ball.position.y > height-100 - ball.radius) {
+    if (ball.position.y > height-40 - ball.radius) {
 			ball.velocity.y *= ball.restitution; //Que bote tendrá
-			ball.position.y = height-100 - ball.radius; //Que no traspase el suelo
+			ball.position.y = height-40 - ball.radius; //Que no traspase el suelo
     }
-    if (ball.position.x > 900 - ball.radius) { //Si se pasa del límite de el campo de golf, vuelve al inicio.
+    if (ball.position.x > 900 - ball.radius || ball.position.y < -200) { //Si se pasa del límite de el campo de golf, vuelve al inicio.
 			ball.velocity.x = 0; //Empieza parada
+			ball.velocity.y = 0;
 			ball.position.x = 100 + ball.radius;
-			ball.position.y = height-100 + ball.radius; 
+			ball.position.y = height-40 + ball.radius; 
     }
     if (ball.position.x < ball.radius) {
 			ball.velocity.x *= ball.restitution; //Rebote pared
 			ball.position.x = ball.radius; 
     }
+	if ( ball.velocity.x < 0.15 && ball.velocity.x > -0.15){
+		ball.velocity.x = 0;
+		ball.stopped = false;
+	}
+	
 
-	
-	
 	
     // Dibujar pelota
     ctx.clearRect(0,0,width,height);
-    
+    onloadfuntzioak();
+	
     ctx.save();
     
     ctx.translate(ball.position.x, ball.position.y);
     ctx.beginPath(); //Que se borre todas las pelotas viejas
     ctx.arc(0, 0, ball.radius, 0, Math.PI*2, true);
     ctx.fill(); //Redibujar la bola
+
     ctx.closePath();
     
     ctx.restore();
 
 
     // Dibujar línea de angulo y potencia
-    if (mouse.isDown) {
+    if (mouse.isDown && ball.velocity.x < 0.1) {
         ctx.beginPath();
         ctx.moveTo(ball.position.x, ball.position.y);
         ctx.lineTo(ball.position.x + (mouse.x - mouseclick.x), ball.position.y + (mouse.y - mouseclick.y));
         ctx.stroke();
         ctx.closePath();
     }
-    
 }
     setup();
