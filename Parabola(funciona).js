929fc60afc0849ba96df88b85d4f2a646808c11e
AnderSantos7 <56338314+AnderSantos7@users.noreply.github.com>
2019-12-03 10:05:15 +0100

    Add files via upload

--- Parabola(funciona).js
+++ Parabola(funciona).js
@@ -5,6 +5,7 @@ var canvas = ctx = false;
 var frameRate = 1/40; // Seconds
 var frameDelay = frameRate * 1000; // ms
 
+
 var img = new Image();
 	img.src = 'api.png';
 var pelota = new Image();
@@ -33,7 +34,7 @@ var bandera = new Image();
  */
  
 var ball = {
-    position: {x: 100, y: height-64},
+    position: {x: 100, y: height-55},
     velocity: {x: 0, y: 0},
     mass: 0.045, //kg
     radius: 4.3, // 1px = 1cm
@@ -75,7 +76,7 @@ function onloadfuntzioak(){
 		width = canvas.width;
 	}
 	
-	ctx.drawImage(pelota, 99, canvas.height-64, 9, 9);
+	ctx.drawImage(pelota, 99, canvas.height-55, 9, 9);
 }
 
 var mouseDown = function(e) {
@@ -115,7 +116,7 @@ var setup = function() {
 //Reducir velocidad cuando no bota
 function rodarsuelo(){
 	
-	if (ball.position.y > height-67 - ball.radius){ //Cuando esté en entre el suelo y la altura de 1px
+	if (ball.position.y > height-58 - ball.radius){ //Cuando esté en entre el suelo y la altura de 1px
 		ball.velocity.x *= 0.97; //Se hace solo x0.97 porque se hace cada 15ms, cuanto mayor sea el intervalo, menor la multiplicación, se haría x 0.95 (por ejemplo)
 	}
 }
@@ -156,20 +157,40 @@ var loop = function() {
     }
 	
     // Collisiones
-    if (ball.position.y > height-64 - ball.radius) {
+	if (ball.position.x < width - 495 && ball.position.x > width - 512 && ball.velocity.x < 0.9 && ball.velocity. x > -0.9 ){
+		if (ball.position.x > width - 495 && ball.position.y < height-55 - ball.radius){
+			ball.velocity.x *= ball.restitution;
+			ball.position.x = width - 495;
+		}
+		
+		if(ball.position.x > width - 512 && ball.position.y < height-55 - ball.radius){
+			ball.velocity.x *= ball.restitution;
+			ball.position.x = width - 512;
+		}
+		if (ball.position.y > height - 27){
+			ball.position.y = height - 27;
+			ball.velocity.x = 0;
+			ball.velocity.y = 0;
+		}
+	}
+    else{
+		if (ball.position.y > height-55 - ball.radius) {
 			ball.velocity.y *= ball.restitution; //Que bote tendrá
-			ball.position.y = height-64 - ball.radius; //Que no traspase el suelo
-    }
-    if (ball.position.x > width - 200 - ball.radius && ball.velocity.x == 0 || ball.position.y < -200 || ball.position.x > width + 400 || ball.position.x < -200 || (ball.position.x < 0 && ball.velocity.x == 0)) { //Si se pasa del límite de el campo de golf, vuelve al inicio.
+			ball.position.y = height-55 - ball.radius; //Que no traspase el suelo
+
+		}
+		if (ball.position.x > width - 200 - ball.radius && ball.velocity.x == 0 || ball.position.y < -200 || ball.position.x > width + 250 || ball.position.x < -200 || (ball.position.x < 0 && ball.velocity.x == 0)) { //Si se pasa del límite de el campo de golf, vuelve al inicio.
 			ball.velocity.x = 0; //Empieza parada
 			ball.velocity.y = 0;
 			ball.position.x = 100 + ball.radius;
-			ball.position.y = height-64 + ball.radius; 
-    }
-	if ( ball.velocity.x < 0.15 && ball.velocity.x > -0.15){
-		ball.velocity.x = 0;
+			ball.position.y = height-55 + ball.radius; 
+		}
+		if ( ball.velocity.x < 0.15 && ball.velocity.x > -0.15){
+			ball.velocity.x = 0;
+		}
 	}
-	if(width !=  window.innerWidth || height !=  window.innerHeight){
+	
+if(width !=  window.innerWidth || height !=  window.innerHeight){
 		if(window.innerWidth < 1000){
 			canvas.width = 1000;
 			canvas.height = window.innerHeight;
@@ -220,8 +241,8 @@ var loop = function() {
         ctx.beginPath();
 		ctx.lineWidth = 2;
 		color();
-        ctx.moveTo(ball.position.x + 20, ball.position.y + 20);
-        ctx.lineTo(ball.position.x + 20 +(mouse.x - mouseclick.x), ball.position.y + 20 +(mouse.y - mouseclick.y));
+        ctx.moveTo(ball.position.x + 15, ball.position.y + 15);
+        ctx.lineTo(ball.position.x + 15 +(mouse.x - mouseclick.x), ball.position.y + 15 +(mouse.y - mouseclick.y));
         ctx.stroke();
         ctx.closePath();
     }
